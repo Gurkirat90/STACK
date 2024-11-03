c31d8ae16b51ac391415b3049650086c9d5a8411
GURKIRAT <gs544448@gmail.com>
2024-11-03 23:59:38 +0530

    removing atsr

---
+++ S5.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string removeStars(string s) 
+    
+    {
+        stack<string>st;
+        for(int i=0;i<s.lenght();i++)
+        {
+            if(s[i]=="*")
+            st.pop();
+            else
+            st.push(s[i]);
+        }
+        string result=" ";
+        stack<string>sr;
+        while(!st.empty())
+        {
+            sr.push(st.top());
+            st.pop();
+            
+        }
+        while(!sr.empty())
+        {
+            st.push(sr.top());
+            result.push_back(st.top());
+            sr.pop();
+        }
+        return result;
+        
+    }
+};
